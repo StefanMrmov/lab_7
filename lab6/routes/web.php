3d2c7938a167a7ec8c47b27b5bf21435b45049aa
StefanMrmov <stefanmrmov@yahoo.com>
2024-12-20 16:40:31 +0100

    fff
    efewf

--- lab6/routes/web.php
+++ lab6/routes/web.php
@@ -7,5 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('categories', CategoryController::class);
-Route::resource('blogs', BlogController::class);
+//Route::resource('categories', CategoryController::class);
+//Route::resource('blogs', BlogController::class);
